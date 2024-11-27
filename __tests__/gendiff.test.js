fe5ab20befa3e955c1d5ba21a6ecbcef98f9371d
forswear <adml90@mail.ru>
2024-11-27 15:25:41 +0700

    correct3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,31 +1,44 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import fs from 'fs';
-import path from 'path';
 import genDiff from '../src/index.js';
 
-const fileExt = ['.json', '.yaml'];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const resultStylish = fs.readFileSync(
-  path.resolve(process.cwd(), '__fixtures__/result_stylish.txt'),
-  'utf-8',
-);
-const resultPlain = fs.readFileSync(
-  path.resolve(process.cwd(), '__fixtures__/result_plain.txt'),
-  'utf-8',
-);
-const resultJson = fs.readFileSync(
-  path.resolve(process.cwd(), '__fixtures__/result_json.txt'),
-  'utf-8',
-);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each(fileExt)('testing different file options', (extension) => {
-  const fileBefore = `__fixtures__/file1${extension}`;
-  const fileAfter = `__fixtures__/file2${extension}`;
-  const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
-  expect(actual1).toEqual(resultStylish);
-  const actual2 = genDiff(fileBefore, fileAfter, 'plain');
-  expect(actual2).toEqual(resultPlain);
-  const actual4 = genDiff(fileBefore, fileAfter);
-  expect(actual4).toEqual(resultStylish);
-  const actual5 = genDiff(fileBefore, fileAfter, 'json');
-  expect(actual5).toEqual(resultJson);
+const filejson1 = getFixturePath('file1.json');
+const filejson2 = getFixturePath('file2.json');
+const fileyaml1 = getFixturePath('file1.yaml');
+const fileyaml2 = getFixturePath('file2.yaml');
+
+test.each([
+  {
+    file1: filejson1, file2: filejson2, expectResult: readFile('result_stylish.txt').trim(),
+  },
+  {
+    file1: fileyaml1, file2: fileyaml2, format: 'stylish', expectResult: readFile('result_stylish.txt').trim(),
+  },
+  {
+    file1: filejson1, file2: filejson2, format: 'stylish', expectResult: readFile('result_stylish.txt').trim(),
+  },
+  {
+    file1: filejson1, file2: filejson2, format: 'json', expectResult: readFile('result_json.txt').trim(),
+  },
+  {
+    file1: fileyaml1, file2: fileyaml2, format: 'json', expectResult: readFile('result_json.txt').trim(),
+  },
+  {
+    file1: filejson1, file2: filejson2, format: 'plain', expectResult: readFile('result_plain.txt').trim(),
+  },
+  {
+    file1: fileyaml1, file2: fileyaml2, format: 'plain', expectResult: readFile('result_plain.txt').trim(),
+  },
+
+])('gendiff %s, %s', ({
+  file1, file2, format, expectResult,
+}) => {
+  expect(genDiff(file1, file2, format)).toBe(expectResult);
 });
